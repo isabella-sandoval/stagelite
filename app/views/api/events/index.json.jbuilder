eae75e5b9f53e46b77c05b0d36c7d68431770e3d
Isabella <isabella.sandoval95@gmail.com>
2020-10-20 16:25:14 -0600

    event ajax/actions

--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -3,8 +3,20 @@
        json.extract! event, :id, :title
         json.date
         json.time
-        
+        json.title
+        json.venue
+        json.address
+        json.latitude
+        json.longitude
+        json.capacity
+        json.price
+        json.age_restriction
+        json.organizer_id
+        json.genre_id
+        json.ticket_link
+        json.at_capacity
 
         json.imageUrl url_for(event.photo)
     end
 end
+
---
+++ frontend/actions/event_actions.js
@@ -0,0 +1,61 @@
+import * as EventApiUtil from '../util/event_api_util';
+
+export const RECEIVE_EVENT = 'RECEIVE_EVENT';
+export const RECEIVE_EVENTS = 'RECEIVE_EVENTS';
+export const RECEIVE_EVENT_ERRORS = 'RECEIVE_EVENT_ERRORS';
+export const REMOVE_EVENT = 'REMOVE_EVENT';
+
+
+export const receiveEvents = events => ({
+    type: RECEIVE_EVENTS,
+    events
+});
+
+
+export const receiveEvent = event => ({
+    type: RECEIVE_EVENT,
+    event
+});
+
+export const receiveErrors = errors => ({
+    type: RECEIVE_EVENT_ERRORS,
+    errors
+});
+
+export const removeEvent = id => ({
+    type: REMOVE_EVENT,
+    id
+})
+
+
+
+
+
+export const fetchEvent = (id) => dispatch => {
+    return EventApiUtil.fetchEvent(id).then(event => 
+        dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON)))
+};
+
+export const fetchEvents = (filters) => dispatch => {
+    return EventApiUtil.fetchEvents(filters).then(events => 
+        dispatch(receiveEvents(events)),
+            errors => dispatch(receiveErrors(errors.responseJSON)))
+};
+
+export const createEvent = (event) => dispatch => {
+    return EventApiUtil.createEvent(event).then(event => 
+        dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON)))
+};
+
+export const updateEvent = (event) => dispatch => {
+    return EventApiUtil.updateEvent(event).then(event => 
+        dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON)))
+};
+
+export const deleteEvent = (id) => dispatch => {
+    return EventApiUtil.deleteEvent(id).then(event => 
+        dispatch(removeEvent(id))
+};
